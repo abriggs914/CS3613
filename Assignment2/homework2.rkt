09a825ef405deeb2c19e4b79c7aebcc418d63127
Avery Briggs <abriggs914@gmail.com>
2019-01-23 01:18:39 -0400

    2019-01-23 working on Assignment2

--- Assignment2/homework2.rkt
+++ Assignment2/homework2.rkt
@@ -20,23 +20,43 @@
   [jpair (key : String) (val : JSON)])
 
 (define-type JSON
-;; ...
   [json (key : String) (val : JPair)]
   [jstring (val : String)]
-  [jarray (val : (Listof Number))]
-  [jboolean (val : JSON)]
-  [true (val : Boolean)]
-  [false (val : Boolean)]
+  [jarray (val : (Listof JSON))]
+  [jboolean (val : Boolean)]
   [jnumber (val : Number)]
-  ;[jdict (val : Dict)]
-)
+  [jdict (val : (Listof JPair))])
 
-#|
+
 ; Question 3
+
+;(define (jpair->string pair)
+;  (....))
+
+(define (json->string val)
+  (type-case JSON val
+    [(json key jpair) (string-append (string-append 
+                      (json->string key) ":") 
+                      (jpair->string jpair))]
+    [(jstring txt) (string-append (string-append 
+                    "\"" txt) "\"")]
+    [(jarray l) (string-append (string-append 
+                 "[" (comma-join to-string l)) "]")]
+    [(jboolean b)  (if b "true" "false")]
+    [(jnumber n) (to-string n)]
+    [(jdict l) (string-append (string-append 
+                "{" (comma-join to-string l))  "}")]))
+
+(define (jpair->string pair)
+  (type-case JPair pair
+    [(jpair key val) (string-append (string-append 
+                      (json->string key) ":") 
+                      (json->string val))]))
 
 
 ;Question 4
-|#
+(define minutes-spent 30)
+
 
 ; Testing
 
@@ -50,30 +70,34 @@
 ; symbols to strings
 (test (comma-join symbol->string (list 'Hello 'World)) "Hello,World")
 
+;; write a lambda test string-append
+
 
 ; Question 2
 (test (jstring? (jstring "hello"))  #t)
-(test (jarray? (jarray (list (jboolean true)
-                           (jnumber 10))))  #t)
-;(test (jdict? (jdict (list (jpair "happy"
-;                                 (jboolean true))
-;                          (jpair "cookies"
-;                                 (jnumber 10)))))  #t)
-#|
-;(test (jpair->string (jpair "Alice" (jstring "Cryptologist")))
-       "\"Alice\":\"Cryptologist\"")
 
+(test (jarray? (jarray (list (jboolean #t)
+                           (jnumber 10))))  #t)
+
+(test (jdict? (jdict (list (jpair "happy"
+                                 (jboolean #t))
+                          (jpair "cookies"
+                                 (jnumber 10)))))  #t)
+
+; Question 3
+
+(test (jpair->string (jpair "Alice" (jstring "Cryptologist")))
+       "\"Alice\":\"Cryptologist\"")
+#|
 (test (json->string
        (jarray (list (jnumber 5)
                      (jnumber 10))))  "[5,10]")
-|#
-#|
-; Question 3
+
 (test (json->string
        (jdict (list (jpair "happy"
-                           (jboolean true))
+                           (jboolean #t))
                     (jpair "crazy"
-                           (jboolean false)))))
+                           (jboolean #f)))))
                            "{\"happy\":true,\"crazy\":false}")
 
 |#
